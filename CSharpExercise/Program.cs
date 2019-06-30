6bd84312ab31bfb6ff5c42e8c5e4d1f27172d4cf
Aleksandar <aleksandar.gujas@gmail.com>
2019-06-30 16:36:19 +1000

    initial commit

---
+++ CSharpExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
